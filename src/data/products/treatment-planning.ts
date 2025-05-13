07d666685c1abeece4e80c33392bb4d71b5be0f4
Matteo Maspero <matteomaspero@users.noreply.github.com>
2025-05-13 23:09:55 +0200

    rev TPS

--- src/data/products/treatment-planning.ts
+++
@@ -1,9 +0,0 @@
-
-import { ProductDetails } from "@/types/productDetails";
-import { RAYSEARCH_TREATMENT_PLANNING_PRODUCTS } from "./treatment-planning/raysearch";
-import { MVISION_TREATMENT_PLANNING_PRODUCTS } from "./treatment-planning/mvision";
-
-export const TREATMENT_PLANNING_PRODUCTS: ProductDetails[] = [
-    ...RAYSEARCH_TREATMENT_PLANNING_PRODUCTS,
-    ...MVISION_TREATMENT_PLANNING_PRODUCTS
-];
